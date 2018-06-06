013260b2d1f5d287a02accdd0f324b6e1017ab77
mikle <mihailgorbunov74@gmail.com>
2018-06-06 16:02:21 -0700

    Some layout changes (for dif. devices)

---
+++ app/src/main/java/com/mikleg/popularmovies/utils/ApiUtils.java
@@ -0,0 +1,17 @@
+package com.mikleg.popularmovies.utils;
+
+/**
+ * Created by mikle on 5/30/2018.
+ * It return the API key
+ */
+
+public class ApiUtils {
+    private static final String apiKey="YOURAPIKEY";
+
+    public ApiUtils() {
+    }
+
+    public static String getApiKey() {
+        return apiKey;
+    }
+}
